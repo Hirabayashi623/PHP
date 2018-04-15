1759766b66072f64cfdebc440341e28cc648c82c
Hirabayashi623 <ryo662233@gmail.com>
2018-04-16 06:46:43 +0900

    auto commit

--- php/xrea/index.php
+++ php/xrea/index.php
@@ -1,205 +1,79 @@
-<!DOCTYPE html>
-<html lang="ja">
+<html>
 <head>
-	<meta name="robots" content="noindex,nofollow" />
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0">
-	<title>Myまとめ</title>
-	<link rel="stylesheet" href="css/index.css" media="screen,print">
-	<script src="js/jquery-3.2.1.min.js" charset="utf-8"></script>
-	<script src="js/index.js" charset="utf-8"></script>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/index.css" rel="stylesheet" />
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/index.js"></script>
+    <title>MyPage</title>
 </head>
 <body>
+<form>
+	<?php
+	include ('config/config.php');
+	// コネクション取得
+	$link = mysqli_connect($host, $user, $pass, $db, $port);
+	if(!$link){
+	    print '<p class="bg-warning">接続に失敗しました</p>';
+	}
 
-	<div class="l_wrap">
-		<section id="demo2" class="section_demo section_demo2">
-			<ul class="section_demo_list">
-				<li>
-					<h3 class="trigger"><a href="#">Python</a></h3>
-					<div class="target">
-						<ul>
-							<li>
-								<h4 class="trigger">
-									<a href="#">機械学習</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="http://gihyo.jp/dev/serial/01/machine-learning" target="_blank">機械学習をはじめよう</a></li>
-										<li><a href="https://prtimes.jp/main/html/rd/p/000000020.000028316.html" target="_blank">寺澤さんより</a></li>
-									</ul>
-								</div>
-							</li>
-							<li>
-								<h4 class="trigger">
-									<a href="#">フレームワーク</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="https://www.sejuku.net/blog/9014" target="_blank">Diango</a></li>
-										<li><a href="#">ITEM 2-2</a></li>
-									</ul>
-								</div>
-							</li>
-							<li>
-								<h4 class="trigger">
-									<a href="#">その他</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="https://prtimes.jp/main/html/rd/p/000000020.000028316.html" target="_blank">寺澤さんより</a></li>
-										<li><a href="#">ITEM 3-2</a></li>
-									</ul>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</li>
+	// クエリ定義
+	$query = 'select m.name as main, s.name as sub, i.name as item, i.url as url ';
+	$query = $query.'from tbl_main_category m, tbl_sub_category s, tbl_items i ';
+	$query = $query.'where m.id = s.parent_id ';
+	$query = $query.'and m.id = i.main_parent_id ';
+	$query = $query.'and s.seq = i.sub_parent_id ';
+	$query = $query.'order by m.id, s.seq, i.seq';
 
-				<li>
-					<h3 class="trigger"><a href="#">サーバー</a></h3>
-					<div class="target">
-						<ul>
-							<li>
-								<h4 class="trigger">
-									<a href="#">XREA</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="https://cp.xrea.com/" target="_blank">コントロールパネル</a></li>
-										<li><a href="https://www.value-domain.com/login.php" target="_blank">ホームページ</a></li>
-									</ul>
-								</div>
-							</li>
-							<li>
-								<h4 class="trigger">
-									<a href="#">Xdomain</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="https://www.xdomain.ne.jp/" target="_blank">ホームページ</a></li>
-										<li><a href="#">ITEM 2-2</a></li>
-										<li><a href="#">ITEM 2-3</a></li>
-										<li><a href="#">ITEM 2-4</a></li>
-									</ul>
-								</div>
-							</li>
-							<li>
-								<h4 class="trigger">
-									<a href="#">SUB CATEGORY 3</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="#">ITEM 3-1</a></li>
-										<li><a href="#">ITEM 3-2</a></li>
-										<li><a href="#">ITEM 3-3</a></li>
-										<li><a href="#">ITEM 3-4</a></li>
-									</ul>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</li>
+    $main_tmp = '';
+    $sub_tmp = '';
+    $isFirstMain = true;
+    $isFirstSub = true;
 
-				<li>
-					<h3 class="trigger"><a href="#">Java</a></h3>
-					<div class="target">
-						<ul>
-							<li>
-								<h4 class="trigger">
-									<a href="#">学習サイト</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="http://jquery.keicode.com/" target="_blank">jQuery入門</a></li>
-										<li><a href="http://www.techscore.com/" target="_blank">TECHSCORE</a></li>
-										<li><a href="https://www.javadrive.jp/start/">JavaDrive</a></li>
-										<li><a href="#">ITEM 1-4</a></li>
-									</ul>
-								</div>
-							</li>
-							<li>
-								<h4 class="trigger">
-									<a href="#">フレームワーク</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="http://www.atmarkit.co.jp/ait/spv/0811/11/news141.html" target="_blank">TERASOLUNA->「Hello World！で理解する、Webアプリ開発の分業化 」</a></li>
-										<li><a href="#">ITEM 2-2</a></li>
-										<li><a href="#">ITEM 2-3</a></li>
-										<li><a href="#">ITEM 2-4</a></li>
-									</ul>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</li>
-
-				<li>
-					<h3 class="trigger"><a href="#">その他</a></h3>
-					<div class="target">
-						<ul>
-							<li>
-								<h4 class="trigger">
-									<a href="#">よく使う</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="https://www.yahoo.co.jp/" target="_blank">Yahoo!</a></li>
-										<li><a href="https://github.com/Hirabayashi623" target="_blank">GitHub</a></li>
-										<li><a href="https://mail.google.com/mail/u/0/#inbox" target="_blank">Gmail</a></li>
-										<li><a href="https://webflow.hra-service.jp/imart/SA1" target="_blank">SISポータル</a></li>
-										<li><a href="https://learning1.hra-service.jp/sa1/" target="_blank">ESS</a></li>
-									</ul>
-								</div>
-							</li>
-							<li>
-								<h4 class="trigger">
-									<a href="#">言語入門</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="http://www.geocities.jp/m_hiroi/golang/index.html" target="_blank">Go言語</a></li>
-										<li><a href="https://www.phpbook.jp/tutorial/" target="_blank">PHP</a></li>
-									</ul>
-								</div>
-							</li>
-							<li>
-								<h4 class="trigger">
-									<a href="#">paiza</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="https://paiza.io/" target="_blank">実行環境</a></li>
-										<li><a href="http://paiza.jp" target="_blank">スキルチェック</a></li>
-									</ul>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</li>
-				<li>
-					<h3 class="trigger"><a href="#">CATEGORY</a></h3>
-					<div class="target">
-						<ul>
-							<li>
-								<h4 class="trigger">
-									<a href="#">SUB_CATEGORY</a>
-								</h4>
-								<div class="target">
-									<ul>
-										<li><a href="#">ITEM</a></li>
-									</ul>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</li>
-			</ul>
-		</section>
-	</div>
+	// クエリ実行
+	$result = mysqli_query($link, $query);
+    ?>
+	<ul class="list-group">
+	<?php while($row = mysqli_fetch_assoc($result)){  ?>
+		<?php if($main_tmp != $row['main']){?>
+			<?php if(!$isFirstMain){
+			    echo '</ul>';    // 直前のサブカテゴリのリスト終了
+			    echo '</li>';    // 自分のアイテム終了
+			    echo '</ul>';    // 直前のサブカテゴリのリスト終了
+                echo '</li>';    // 自分のアイテム終了
+			}else{
+			    $isFirstMain = false;
+			}?>
+    		<li class="list-group-item">
+    			<h4 class="triger"><?php echo $row['main']; $main_tmp = $row['main'];?></h4>
+    				<ul class="list-group target">
+		<?php $isFirstSub = true;}?>
+		<?php if($sub_tmp!=$row['sub']){?>
+    		<?php
+    		if(!$isFirstSub){
+    		    echo '</ul>';     // 直前のリンクリスト終了
+                echo '</li>';     // 自分のアイテム終了
+    		}else{
+    		    $isFirstSub = false;
+    		}
+    		?>
+    		<li class="list-group-item"><h5 class="triger"><?php echo $row['sub']; $sub_tmp = $row['sub']?></h5>
+    			<ul class="list-group target">
+    	<?php }?>
+    	<li class="list-group-item"><a href="<?php echo $row['url']?>" target="_blank"><?php echo $row['item']?></a></li>
+	<?php } ?>
+	</ul>	<!-- サブカテゴリのリスト終了 -->
+	</li>	<!-- メインカテゴリのアイテム終了 -->
+	</ul>	<!-- メインカテゴリのリスト終了 -->
 
 
-
+	<?php
+	// 接続断
+	mysqli_close($link);
+	?>
+</form>
 </body>
 </html>
--- php/xrea/js/index.js
+++ php/xrea/js/index.js
@@ -1,36 +1,8 @@
-$(function() {
-
-	var activeClass = 'active';
-
-	// ハンバーガーボタン
-	function hamburgBtn() {
-		$('.btn_hamburger').on('click', function(event) {
-			$button = $(this);
-			event.preventDefault();
-
-			$button.find('a').toggleClass(activeClass);
-			$button.next().slideToggle(250);
-		});
-	};
-
-	// DEMO 1, DEMO 2
-	function demo1_2(triggerSelector) {
-		// 引数で指定したトリガーをクリックしたとき、クリックしたトリガーの次の要素を展開
-		$(triggerSelector).on('click', function(event) {
-			$trigger = $(triggerSelector);
-			event.preventDefault();
-
-			$(this).toggleClass(activeClass);
-			$(this).next().slideToggle(250);
-		});
-	};
-
-	// 実行
-	hamburgBtn();
-
-	// 引数でトリガーを指定
-	demo1_2('#demo1 .trigger');
-	demo1_2('#demo2 .trigger');
-	demo3('#demo3 .trigger');
-
+$(function(){
+	$('.triger').on({
+		'click': function(){
+			$(this).next('.target').slideToggle(300);
+			$(this).parent().toggleClass('list-group-item-info');
+		}
+	});
 });
